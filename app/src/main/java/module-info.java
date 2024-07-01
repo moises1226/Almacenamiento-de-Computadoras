c1673c46fb5fe1bdc9dec9bf695e0a31e331b6cd
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-06-30 22:32:21 -0300

    commit1

---
+++ app/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module sm.app {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens sm.app to javafx.fxml;
+    exports sm.app;
+}
