3505caa06f75faa1eed7d867a15befc8b9436c4e
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-07-04 11:21:54 -0300

    creacion de las estructuras del panel,

--- app/src/main/java/sm/app/HelloApplication.java
+++ app/src/main/java/sm/app/HelloApplication.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("PanelPrincipal.fxml"));
+        Scene scene = new Scene(fxmlLoader. load(), 1016, 687);
+        stage.setTitle("Guardado de Computadoras");
         stage.setScene(scene);
         stage.show();
     }
--- app/src/main/java/sm/app/HelloController.java
+++ app/src/main/java/sm/app/HelloController.java
@@ -1,14 +1,86 @@
 package sm.app;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button btn_Table;
+    @FXML
+    private TableView tabla;
+
+    @FXML
+    public void Mostrar_tabla(){
+
+        if(tabla.isVisible()){
+
+            tabla.setVisible(false);
+        }else{
+
+            tabla.setVisible(true);
+
+        }
+
+
     }
+
+
+    @FXML
+    private Spinner<Integer> nro_carrito;
+
+
+    @FXML
+    public void initialize() {
+        // Configura el Spinner con un rango de valores
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100000, 0);
+        nro_carrito.setValueFactory(valueFactory);
+
+
+    }
+
+    @FXML
+    private TextField nombre_Respo;
+    private TextField curso;
+
+    @FXML
+    private Spinner<Integer> nro_compu;
+
+    @FXML
+    private void btn_Enviar(){
+
+        String nombre = nombre_Respo.getText();
+        String curso = this.curso.getText();
+        int nro_Carrito = this.nro_carrito.getValue();
+        int nro_compu = this.nro_compu.getValue();
+
+        GuardadoDeDatos(nombre, curso, nro_Carrito, nro_compu);
+
+
+    }
+
+
+    @FXML
+    public void GuardadoDeDatos(String nombre, String curso, int nro_Carrito, int nro_compu ) {
+
+        ConectorBaseDatos conexion = new ConectorBaseDatos();
+
+        try {
+
+
+
+
+
+        }catch (Exception e){}
+
+    }
+
+
+
+
+
+
+
 }
