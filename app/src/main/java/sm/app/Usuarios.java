80dea40f957078f760828b04f07bbf474f37b0c2
Moises Aguilar <moises.aguilar.et7@gmail.com>
2024-07-08 00:37:01 -0300

    agregado de nuevos parámetros

--- app/src/main/java/sm/app/Usuarios.java
+++ app/src/main/java/sm/app/Usuarios.java
@@ -5,11 +5,19 @@ public class Usuarios {
     private int id;
     private String nombre;
     private String apellido;
+    private String curso;
+    private int nro_compu;
+    private String descripcion;
+    private long nro_Carrito;
 
-    public Usuarios(int id, String nombre, String apellido) {
+    public Usuarios(int id, String nombre, String apellido , String curso, int nro_compu, String descripcion, long nro_Carrito) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
+        this.curso = curso;
+        this.nro_compu = nro_compu;
+        this.descripcion = descripcion;
+        this.nro_Carrito = nro_Carrito;
     }
     public int getId() {
         return id;
@@ -34,4 +42,45 @@ public class Usuarios {
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-}
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public int getNro_compu() {
+        return nro_compu;
+    }
+
+    public void setNro_compu(int nro_compu) {
+        this.nro_compu = nro_compu;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+
+            this.descripcion = descripcion;
+
+    }
+
+    public long getNro_Carrito() {
+        return nro_Carrito;
+    }
+    public void setNro_Carrito(long nro_Carrito) {
+        this.nro_Carrito = nro_Carrito;
+
+    }
+
+
+
+    }
+
+
+
+
