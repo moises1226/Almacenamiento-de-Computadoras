dbca319be86e2ad5aaa5038946bcf176a4d75fcb
Moises <moises.aguilar.et7@gmail.com>
2024-10-18 00:33:37 -0300

    modificacion

--- app/src/main/java/sm/app/APP.java
+++ app/src/main/java/sm/app/APP.java
@@ -3,6 +3,7 @@ package sm.app;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -16,9 +17,13 @@ public class APP extends Application {
 
         FXMLLoader fxmlLoader = new FXMLLoader(APP.class.getResource("PanelPrincipal.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1182, 680);
+        Image icon = new Image(APP.class.getResourceAsStream("icono.png")); // Reemplaza "icono.png" por el nombre de tu archivo
+        stage.getIcons().add(icon); // Agrega el ícono al Stage
         stage.setTitle("Guardado de Computadoras");
         stage.setScene(scene);
         stage.setResizable(false);
+
+
         stage.show();
 
 
